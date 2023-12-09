357bb3a2303eff48f3c08466583d83fc43a0dad8
Jesus Villalobos Jr <jesus.villalobosjr@gmail.com>
2023-12-08 15:07:06 -1000

    Add functionality to add book as an element in the DOM

--- index.js
+++ index.js
@@ -28,12 +28,56 @@ function clearInput(){
     read.checked = false;
 }
 
-function submitClicked(e){
+function changeReadStatus(e){
+    const status = e.target.textContent;
+    if(status === "Not Read"){
+        e.target.textContent = "Read";
+        e.target.style.backgroundColor = green;
+    }else{
+        e.target.textContent = "Not Read";
+        e.target.style.backgroundColor = red;
+    }
+}
+
+function deleteBook(e){
+    const button = e.target;
+    const bookToDelete = document.querySelector(`.${button.id}`);
+    books.removeChild(bookToDelete);
+}
+
+function createBookElement(bookObj){
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book");
+    const bookID = `bookID-${id++}`;
+    bookElement.classList.add(bookID);
+    const bookTitle = document.createElement("p");
+    bookTitle.textContent = `"${bookObj.title}"`;
+    bookElement.appendChild(bookTitle)
+    const bookAuthor = document.createElement("p");
+    bookAuthor.textContent = bookObj.author;
+    bookElement.appendChild(bookAuthor);
+    const bookPages = document.createElement("p");
+    bookPages.textContent = bookObj.pages;
+    bookElement.appendChild(bookPages);
+    const bookRead = document.createElement("button");
+    bookRead.textContent = (bookObj.read ? "Read" : "Not Read");
+    bookRead.style.backgroundColor = (bookObj.read ? green : red);
+    bookRead.addEventListener("click",changeReadStatus)
+    bookElement.appendChild(bookRead);
+    const deleteButton = document.createElement("button");
+    deleteButton.id = bookID;
+    deleteButton.textContent = "Remove";
+    deleteButton.addEventListener("click",deleteBook);
+    bookElement.appendChild(deleteButton);
+    books.appendChild(bookElement);
+}
+
+function bookSubmitted(e){
     e.preventDefault();
     const book = getBookAdded();
     clearInput();
     hideModalSubmit();
-    console.log(book);
+    createBookElement(book);
 }
 
 function getBookAdded(){
@@ -52,10 +96,14 @@ const title = document.querySelector(".book-title");
 const author = document.querySelector(".book-author");
 const pages = document.querySelector(".book-pages");
 const read = document.querySelector(".read");
+const books = document.querySelector(".books");
+const red = "rgb(170,0,0)";
+const green = "lightgreen";
 let onModal = false;
+let id = 0;
 
 addBook.addEventListener("click",displayModal);
 modal.addEventListener("click",hideModal);
 modalContent.addEventListener("mouseover",() => onModal = true);
 modalContent.addEventListener("mouseout",() => onModal = false);
-submitButton.addEventListener("click",submitClicked);
+submitButton.addEventListener("click",bookSubmitted);
